707b1a45a0129efe8c1992933e273dca04e614c2
kubraguness <kubragunes@madde22.com>
2022-06-07 21:41:50 +0300

    auth components done

---
+++ store-app/src/components/loginPageComponent/loginPageComponent.jsx
@@ -0,0 +1,68 @@
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { login } from "../../store/authStore";
+
+const LoginPageComponent = (props) => {
+  const dispatch = useDispatch();
+
+  const [inputs, setInputs] = useState({});
+
+  const loginUser = {
+    username: "",
+    password: "",
+  };
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(login({ inputs: inputs }));
+    alert(inputs);
+  };
+
+  // useEffect(() => {
+  //   //  console.log(inputs);
+  // }, [inputs]);
+
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: "center" }}>Giriş Yap</h1>
+      <div className="card">
+        <div className="contianer m-2">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>Kullanıcı Adı:</label>
+              <input
+                className="form-control"
+                type="text"
+                name="username"
+                value={inputs.username || ""}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Şifre:</label>
+              <input
+                className="form-control"
+                type="text"
+                name="password"
+                value={inputs.password || ""}
+                onChange={handleChange}
+              />
+              <button className="btn btn-success mt-2" type="submit">
+                Giriş Yap
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPageComponent;
---
+++ store-app/src/components/signUpPageComponent/signUpPageComponent.jsx
@@ -0,0 +1,113 @@
+import { useEffect, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addUser } from "../../store/authStore";
+
+const SignUpPageComponent = (props) => {
+  const dispatch = useDispatch();
+
+  const [inputs, setInputs] = useState({});
+
+  const newUser = {
+    id: 0,
+    username: "",
+    fullName: "",
+    email: "",
+    role: "",
+    password: "",
+  };
+
+  const handleChange = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setInputs((values) => ({ ...values, [name]: value }));
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addUser({ inputs: inputs }));
+    alert(inputs);
+  };
+
+  return (
+    <div className="container">
+      <h1 style={{ textAlign: "center" }}>Kayıt Ol</h1>
+
+      <div className="card">
+        <div className="container m-2">
+          <form onSubmit={handleSubmit}>
+            <div className="form-group">
+              <label>Kullanıcı ID:</label>
+              <input
+                className="form-control"
+                type="text"
+                name="id"
+                value={inputs.id || ""}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Kullanıcı Adı:</label>
+              <input
+                className="form-control"
+                type="text"
+                name="username"
+                value={inputs.username || ""}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Kullanıcı Tam Adı:</label>
+              <input
+                className="form-control"
+                type="text"
+                name="fullName"
+                value={inputs.fullName || ""}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Kullanıcı Mail:</label>
+              <input
+                className="form-control"
+                type="text"
+                name="email"
+                value={inputs.email || ""}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Kullanıcı Rolü:</label>
+              <input
+                className="form-control"
+                type="text"
+                name="role"
+                value={inputs.role || ""}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className="form-group">
+              <label>Kullanıcı Şifresi:</label>
+              <input
+                className="form-control"
+                type="password"
+                name="password"
+                value={inputs.password || ""}
+                onChange={handleChange}
+              />
+              <button className="btn btn-success mt-2" type="submit">
+                Kayıt ol
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default SignUpPageComponent;
